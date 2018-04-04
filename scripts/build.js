eaecc3bc55073f6097b8410c387e7ff54b3b45c4
Johann Behr <johannbehr@exng.de>
2018-04-04 19:21:41 +0200

    Added build script

---
+++ scripts/build.js
@@ -0,0 +1,29 @@
+// IMPORTS
+var sass = require('node-sass')
+var path = require('path')
+var fs = require('fs')
+
+// VARIABLES
+var inPath = path.join(__dirname, '..', 'src', 'antimatter.scss')
+var outPath = path.join(__dirname, '..', 'dist', 'antimatter.css')
+
+// HELPER
+const mkdirSync = function (dirPath) {
+  try {
+    fs.mkdirSync(dirPath)
+  } catch (err) {
+    if (err.code !== 'EEXIST') throw err
+  }
+}
+
+// BUILD
+var result = sass.render({
+  file: inPath,
+  outFile: outPath
+}, function (err, out) {
+  if (err) throw err
+
+  mkdirSync(path.join(outPath, '..'))
+
+  fs.writeFileSync(outPath, out.css)
+})
